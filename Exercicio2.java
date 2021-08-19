f17f1d7c694057ee353c8c4d889d8294a83ff78a
rodolfopmo <rodolfo.pmo@gmail.com>
2021-08-19 20:00:22 +0000

    Código Exercício 2

---
+++ Exercicio2.java
@@ -0,0 +1,10 @@
+public class Exercicio2{
+    public static void main(String args []){
+        int soma = 0; 
+        for(int i = 1; i <= 1000; i++){
+            soma = soma + i;     
+            
+        }
+        System.out.println("A soma de 1 a 1000 é: " + soma);
+    }
+}
